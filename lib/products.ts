1f6806760ab4745d90e9a9ed0e8e7c81c7daf7d9
wencountach <wencountach@gmail.com>
2022-04-05 20:39:51 -0700

    Changed frontend.

--- lib/products.ts
+++ lib/products.ts
@@ -1,16 +1,16 @@
 export const products = [
   {
-    id: 'box-of-cookies',
+    id: 'box-of-monkes',
     name: 'Box',
-    description: 'A delicious box of handmade cookies',
+    description: 'A delicious box of Solana monkes',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
+    id: 'basket-of-monkes',
     name: 'Basket',
-    description: 'A large basket of handmade cookies',
+    description: 'A large basket of Solana monkes',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
