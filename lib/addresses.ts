2820850ded7b956bf41995e226539c846438ce11
wencountach <wencountach@gmail.com>
2022-04-07 18:12:24 -0700

    Solana Pay working

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,6 +1,6 @@
 import { PublicKey } from "@solana/web3.js"
 
 // this is your shop wallet address
-export const shopAddress = new PublicKey('...')
+export const shopAddress = new PublicKey('BcL329obKVA3Q3eS3nDWqESQ67dmYLhRuh8L4gC9HgYh')
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
